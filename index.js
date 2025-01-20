1e573ba2425339aaa8c20639849980ff67b80804
Ashwinram005 <ashwinrammohan2005@gmail.com>
2025-01-20 23:16:29 +0530

    completed filesystem

--- index.js
+++ index.js
@@ -19,8 +19,8 @@
 // console.log(JSON.stringify(tesla,null,2))
 // console.log(JSON.stringify(ford,undefined,2));
 
-// const path=require("path");
-// const filePath=path.join(__dirname,"sample.txt");
+const path=require("path");
+const filePath=path.join(__dirname,"files","sample.txt");
 // //dirname
 // console.log(path.dirname(filePath));
 // console.log(__dirname)
@@ -30,3 +30,61 @@
 // //extension
 // console.log(path.extname(filePath));
 
+const fs=require("fs")
+const fsPromises=require("fs").promises
+// // Reading from a file-async
+// fs.readFile(filePath,"utf-8",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data);
+// })
+
+// //Reading from a file-sync
+// try {
+//     const data=fs.readFileSync(filePath,"utf-8");
+//     console.log(data);
+// } catch (error) {
+//     console.log(error);
+// }
+
+// const fileReading=async()=>{
+//     try {
+//         const data=await fsPromises.readFile(filePath,"utf-8");
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// fileReading()
+
+//writing into file
+const txtfile=path.join(__dirname,"files","text.txt")
+// const content="I am developer";
+// fs.writeFile(txtfile,content,(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("Writed successfully");
+//     fs.readFile(txtfile,"utf-8",(err,data)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log(data);
+//     })
+// })
+
+const writingInFile=async()=>{
+    try {
+        await fsPromises.writeFile(txtfile,"Hello",{
+            flag:"a+"
+        });
+        // await fsPromises.appendFile(txtfile,"\nWelcome");
+        await fsPromises.rename(txtfile,path.join(__dirname,"files","newtxt.txt"));
+        const data=await fsPromises.readFile(path.join(__dirname,"files","newtxt.txt"),"utf-8");
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+writingInFile()
